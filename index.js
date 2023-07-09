2afd0778b03c24f6433190226f4aa46f79d3f899
Atlas_Kaplan <venatic.kaplan@gmail.com>
2023-07-09 10:32:22 -0600

    update

---
+++ index.js
@@ -0,0 +1,12 @@
+const inputName = document.querySelector('.input-name');
+const inputNumber = document.querySelector('.input-number');
+const inputMonth = document.querySelector('.input-month');
+const inputYear = document.querySelector('.input-year');
+const inputCVC = document.querySelector('.input-cvc');
+const cardName = document.querySelector('.card-name');
+const cardNumber = document.querySelector('.card-number');
+const cardMonth = document.querySelector('.card-date');
+const cardYear = document.querySelector('.card-year');
+const cardCVC = document.querySelector('.card-pin');
+const form = document.querySelector('.form');
+
