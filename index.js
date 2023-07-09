0f361d59689ac2b79f8b4be6e1ac98c0f1b66d53
Atlas_Kaplan <venatic.kaplan@gmail.com>
2023-07-09 14:04:06 -0600

    card interaction w DOM

--- index.js
+++ index.js
@@ -10,3 +10,42 @@ const cardYear = document.querySelector('.card-year');
 const cardCVC = document.querySelector('.card-pin');
 const form = document.querySelector('.form');
 
+inputName.addEventListener('input', ()=> {
+    cardName.innerText = inputName.value;
+
+    if (inputName.value.length === 0) {
+        cardName.innerText = "Jane Appleseed";
+    }
+})
+
+inputNumber.addEventListener('input', () => {
+    cardNumber.innerText = inputNumber.value;
+
+    if (inputNumber.value.length === 0) {
+        cardName.innerText = "0000 0000 0000 0000";
+    }
+})
+
+inputMonth.addEventListener('input', () => {
+    cardMonth.innerText = inputMonth.value;
+
+    if (inputMonth.value.length === 0) {
+        cardMonth.innerText = "00";
+    }
+})
+
+inputYear.addEventListener('input', () => {
+    cardYear.innerText = inputYear.value;
+
+    if (inputYear.value.length === 0) {
+        cardYear.innerText = "00";
+    }
+})
+
+inputCVC.addEventListener('input', () => {
+    cardCVC.innerText = inputCVC.value;
+
+    if (inputCVC.value.length === 0) {
+        cardCVC.innerText = "000";
+    }
+})
