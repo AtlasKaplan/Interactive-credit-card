9dafc766e93bd12ae184b72811941c26ed4ceb9a
Atlas_Kaplan <venatic.kaplan@gmail.com>
2023-07-10 09:01:28 -0600

    js update

--- index.js
+++ index.js
@@ -11,6 +11,29 @@ const cardCVC = document.querySelector('.card-pin');
 
 const form = document.querySelector('.form');
 
+const numberError = document.querySelector('#number-error');
+const nameError = document.querySelector('#name-error');
+const monthError = document.querySelector('#month-error');
+const cvcError = document.querySelector('#cvc-error');
+
+// function space(el, after) {
+//     after = after || 4;
+//     var v = el.value.replace(/[^\dA-Z]/g, ''),
+//         reg = new RegExp(".{" + after + "}", "g")
+//     el.value = v.replace(reg, function (a, b, c) {
+//         return a + ' ';
+//     });
+// }
+
+// var el = document.getElementById('input-number');
+// el.addEventListener('keyup', function () {
+//     space(this, 4);
+// });
+
+// var cleave = new Cleave('input-number', {
+//     creditCard: true,
+// });
+
 inputName.addEventListener('input', ()=> {
     cardName.innerText = inputName.value;
 
@@ -51,6 +74,67 @@ inputCVC.addEventListener('input', () => {
     }
 })
 
-var cleave = new Cleave('.input-number', {
-    creditCard: true,
+
+
+
+
+
+
+form.addEventListener('click', (e) => {
+    e.preventDefault();
+
+
+if (inputName.value.length == 0){
+    nameError.classList.remove('hide');
+    inputName.classList.add('input-error');
+}
+else {
+    nameError.classList.add('hide');
+    inputName.classList.remove('input-error');
+}
+
+
+
+
+const numberI = inputNumber.value;
+    const numberRegex = /^[0-9\s]*$/;
+
+if (!numberRegex.test(numberI)) {
+    numberError.classList.remove('hide');
+    inputNumber.classList.add('input-error');
+} 
+else {
+    numberError.classList.add('hide');
+    inputNumber.classList.remove('input-error');
+}
+
+
+if (inputMonth.value.length == 0) {
+    monthError.classList.remove('hide');
+    inputMonth.classList.add('input-error');
+}
+else {
+    monthError.classList.add('hide');
+    inputMonth.classList.remove('input-error');
+}
+
+if (inputYear.value.length == 0) {
+    monthError.classList.remove('hide');
+    inputYear.classList.add('input-error');
+}
+else {
+    monthError.classList.add('hide');
+    inputYear.classList.remove('input-error');
+}
+
+if (inputCVC.value.length == 0) {
+    cvcError.classList.remove('hide');
+    inputCVC.classList.add('input-error');
+}
+else {
+    cvcError.classList.add('hide');
+    inputCVC.classList.remove('input-error');
+}
+
 });
+
